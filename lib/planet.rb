9817eac4da7c28dd7ed2c8d6fac57290335963c1
Robert Korzeniec <r.korzeniec@gmail.com>
2015-09-22 19:06:17 +0200

    Basic planet class implementation

---
+++ lib/planet.rb
@@ -0,0 +1,52 @@
+class Planet
+	attr_reader :x, :y, :obstacles
+
+	def initialise(x = 30, y = 30)
+		@x = x
+		@y = y
+		@obstacles = Array.new
+		i = 0
+		rand(5...x).each do
+			rand_x = rand(0...x)
+			rand_y = rand(0...y)
+			unless obstacle_at?(rand_x, rand_y)
+				obstacle[i] = [rand_x, rand_y]
+				i += 1
+			end
+		end
+	end
+
+	def obstacle_at?(x, y)
+		isObstacle = false
+		@obstacles.each do |obstacle|
+			if obstacle[:x] == x && obstacle[:y] == y
+				isObstacle = true
+				break
+			end
+		end
+	end
+
+	def actual_location(x, y)
+		new_x = case x 
+			when x > @x then 0
+			when x < 0 then @x
+			else x
+		end
+
+		new_y = case y 
+			when y > @y then 0
+			when y < 0 then @y
+			else y
+		end
+	end
+
+	def to_s(rover_x, rover_y)
+		planet = "Size -> x:#{@x} y:#{@y}\n\n"
+		for i = @y in @y..0
+			planet += "#{i} |"
+			#for j = @x
+
+			i -= 1
+		end
+	end
+end
